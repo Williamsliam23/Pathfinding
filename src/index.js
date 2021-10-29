12bf014539a3f76d08c4ef5238806ed2c6a5d098
Williamsliam23 <williams.liam23@gmail.com>
2021-10-29 18:13:55 -0500

    canvas html element drawing testing and styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { default as PathCanvas } from "./canvas";
 
 window.addEventListener('DOMContentLoaded', () => {
   const can = document.getElementById('canvas')
+  can.style.border = '2px solid black'
   const test = new PathCanvas (can) ;
   test.draw();
 })
